e48354972c5ca6f84e581e0668dc24f0dd349aad
hibikisameta <hibikisameta@gmail.com>
2017-12-13 10:41:05 +0900

    PrimeNumberプログラム作成

---
+++ src/elementProgram/PrimeNumber.java
@@ -0,0 +1,30 @@
+package elementProgram;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PrimeNumber {
+	// PrimeNumberクラスを使う
+
+	public static void main(String[] args) throws IOException {
+		// TODO 自動生成されたメソッド・スタブ
+
+	BufferedReader br =
+	new BufferedReader (new InputStreamReader (System.in));
+
+	System.out.println("PrimeNumber");
+	// PrimeNumberと出す
+
+	string str = br.readLine();
+	int res = Integer.parseInt(str);
+
+	boolean s = true;
+	//boolean型の変数sはtrue
+
+	for(int i = 1; i <= res / 2; i++){
+	// 1 ～ 入力された値になるまで、+1を繰り返す
+
+	}
+}
+}
