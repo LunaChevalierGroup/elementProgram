59fa7d69d997f25bd24d36de573f25953f64a7cb
hibikisameta <hibikisameta@gmail.com>
2017-12-28 18:59:25 +0900

    出力機能追加

--- src/elementProgram/FileManager.java
+++ src/elementProgram/FileManager.java
@@ -1,11 +1,15 @@
 package elementProgram;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class FileManager {
 
@@ -13,11 +17,16 @@ public class FileManager {
 		// TODO 自動生成されたメソッド・スタブ;
 
 		try {
-			File f = new File ("C:\\保存用\\CustomerInfo.csv") ;
-			FileReader fr = new FileReader (f);
-			FileInputStream fis = new FileInputStream(f);
-			InputStreamReader isr = new InputStreamReader(fis,"UTF-8");
+			File inputfile = new File ("src/resource/CustomerInfo.csv");
+			File outputfile = new File ("src/resource/CustomerInfoOutPut.csv");
+			FileReader fr = new FileReader (inputfile);
+			FileWriter fw = new FileWriter (outputfile);
+			FileInputStream fis = new FileInputStream (inputfile);
+			FileOutputStream fos = new FileOutputStream (outputfile);
+			InputStreamReader isr = new InputStreamReader (fis);
+			OutputStreamWriter osw = new OutputStreamWriter (fos);
 			BufferedReader br = new BufferedReader (fr);
+			BufferedWriter bw = new BufferedWriter (fw);
 
 			String line;
 			int i = 0;
@@ -25,11 +34,12 @@ public class FileManager {
 				System.out.println((i++ + " ") + line);
 			}
 			br.close();
+			bw.close();
 			isr.close();
+			osw.close();
 
 		} catch (IOException e) {
 			System.out.println(e);
 		}
 	}
-
 }
