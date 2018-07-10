52e6a54a949d3346b62a919b7887056d285c72c8
sasaKK <awankoromochi@gmail.com>
2018-07-10 09:20:06 +0900

    FizzBuzzと素数判定を追加しました。

---
+++ src/samplePackage/sosuu.java
@@ -0,0 +1,19 @@
+package samplePackage;
+import java. util.Scanner;
+public class sosuu {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	 System.out.println("数字を入力して下さい。 ");
+     int n = sc.nextInt();
+if( n == 1){
+	System.out.println( n + "は素数ではありません。");
+}else if( n % n == 0 && n % 1 == 0);
+System.out.println( n + "は素数です。");
+	 if( n < 1);
+	System.out.println( n + "は素数ではありません");
+
+
+
+}
+
+}
