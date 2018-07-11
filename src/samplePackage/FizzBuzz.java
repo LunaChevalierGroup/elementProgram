be7686d6575dd1c8fdd80808586c1d0131ac7ba1
sasaKK <awankoromochi@gmail.com>
2018-07-11 13:05:17 +0900

    FizzBuzz完成

--- src/samplePackage/FizzBuzz.java
+++ src/samplePackage/FizzBuzz.java
@@ -4,43 +4,25 @@ import java.util.Scanner;
 
 public class FizzBuzz {
 	public static void main(String[] args) {
-int i;
-Scanner sc = new Scanner(System.in);
-System.out.println("数字を入力してください。");
-int num = sc.nextInt();
-sc.close();
-for(i = 0; i <= num; i ++);
-if( i % 3 == 0 && i % 5 == 0  )
-System.out.println("FizzBuzz");
-else if( i % 3 == 0)
-		System.out.println("Fizz");
-		else if ( i % 5 == 0)
-System.out.println("Buzz");
+		int i;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("数字を入力してください。");
+		int num = sc.nextInt();
+		sc.close();
+
+		for (i = 0; i <= num; i++) {
+
+		if (i % 3 == 0 && i % 5 == 0)
+			System.out.println("FizzBuzz");
+		else if (i % 3 == 0)
+			System.out.println("Fizz");
+		else if (i % 5 == 0)
+			System.out.println("Buzz");
 		else
 			System.out.println(i);
 
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
-
-
-
-
-
+		}
 
 	}
 
-
-
-
+}
