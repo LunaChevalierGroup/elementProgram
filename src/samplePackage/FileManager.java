b3e4fa0bf860a095c9ab17bdc345e5ef7c791102
dfjkyrcq <te.yuusuke.130.ushio.350@gmail.com>
2018-07-18 16:26:22 +0900

    ファイル入出力修正中

--- src/samplePackage/FileManager.java
+++ src/samplePackage/FileManager.java
@@ -1,28 +1,39 @@
 package samplePackage;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class FileManager {
 	public static void main(String args[]) {
 		try {
-			File f = new File("CSV.csv");
-			BufferedReader br = new BufferedReader(new FileReader(f));
-			
+			File csv = new File("CSV.csv");
+			BufferedReader br = new BufferedReader(new FileReader(csv));
+			ArrayList<String> list = new ArrayList<String>();
 			String line;
-			for (int i = 1; (line = br.readLine()) != null; i++) {
-				String[] data = line.split(",", 0);
-				System.out.print(i);
-				for (String elem : data) {
-					System.out.print(elem);
-				}
-				System.out.println();
+			int i = 1;
+			while ((line = br.readLine()) != null) {
+				System.out.println(i + "," + line);
+				list.add(i + "," + line);
+				i++;
 			}
+			BufferedWriter bw = new BufferedWriter(new FileWriter(csv, false));
+			for (String mes : list) {
+				bw.write(mes);
+				bw.newLine();
+			}
+
+			bw.close();
 			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
-			System.out.println(e);
+			e.printStackTrace();
 		}
 	}
 }
