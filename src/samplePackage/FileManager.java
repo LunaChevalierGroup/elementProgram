882f12aec640283c0a05b89ccd7bdef80ea64bcd
dfjkyrcq <te.yuusuke.130.ushio.350@gmail.com>
2018-07-20 16:02:37 +0900

    ファイル入出力課題クラス分け

--- src/samplePackage/FileManager.java
+++ src/samplePackage/FileManager.java
@@ -8,22 +8,21 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class FileManager {
 	public static void main(String args[]) {
 		try {
 			File csv = new File("CSV.csv");
 			BufferedReader br = new BufferedReader(new FileReader(csv));
-			ArrayList<String> list = new ArrayList<String>();
-			String line;
-			int i = 1;
-			while ((line = br.readLine()) != null) {
-				System.out.println(i + "," + line);
-				list.add(i + "," + line);
-				i++;
+
+			Editor edi = new Editor();
+
+			while ((edi.line = br.readLine()) != null) {
+				edi.editor();
 			}
 			BufferedWriter bw = new BufferedWriter(new FileWriter(csv, false));
-			for (String mes : list) {
+			for (String mes : edi.list) {
 				bw.write(mes);
 				bw.newLine();
 			}
@@ -36,4 +35,18 @@ public class FileManager {
 			e.printStackTrace();
 		}
 	}
+}
+
+class Editor {
+	List<String> list = new ArrayList<String>();
+	String line;
+	int i = 1;
+
+	void editor() {
+		System.out.println(i + "," + line);
+		list.add(i + "," + line);
+		i++;
+
+	}
+
 }
