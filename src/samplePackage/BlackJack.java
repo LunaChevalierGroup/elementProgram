530e83796421be331efb256d9603ea38df72b7c4
dfjkyrcq <te.yuusuke.130.ushio.350@gmail.com>
2018-07-26 16:58:45 +0900

    7月26日の成果物

--- src/samplePackage/BlackJack.java
+++ src/samplePackage/BlackJack.java
@@ -12,40 +12,53 @@ public class BlackJack {
 		List<Tramp> plist = new ArrayList<Tramp>();
 		List<Tramp> dlist = new ArrayList<Tramp>();
 		Tramp[] tramp = Tramp.values();
-
+		int pp = 0;
+		int dp = 0;
 		for (int i = 0; i < tramp.length; i++) {
 			tlist.add(tramp[i]);
 		}
+
 		for (int i = 1; i <= 2; i++) {
 			Tramp player = tlist.remove(0);
 			plist.add(player);
+			pp = pp + plist.remove(0).getTrampNum();
 			System.out.println(i + "枚目プレイヤーは" + player + "を引きました");
-		}
-		for (int i = 1; i <= 2; i++) {
+
 			if (i == 2) {
 				Tramp dealer = tlist.remove(0);
 				dlist.add(dealer);
+				dp = dp + dlist.remove(0).getTrampNum();
 				System.out.println(i + "枚目ディーラーが引いたカードは非表示です。");
 			} else {
 				Tramp dealer = tlist.remove(0);
 				dlist.add(dealer);
+				dp = dp + dlist.remove(0).getTrampNum();
 				System.out.println(i + "枚目ディーラーは" + dealer + "を引きました");
 			}
 		}
-		System.out.println("カードを引きますか？（はい：Yを入力、いいえ：それ以外を入力）");
-		String select = sc.nextLine();
-		if(select=="Y"){
+
+		System.out.println(pp);
+		System.out.println("カードを引きますか？（はい：yを入力、いいえ：それ以外を入力）");
+		while (sc.next().equals("y")) {
+
 			Tramp player = tlist.remove(0);
 			plist.add(player);
+			pp = pp + plist.remove(0).getTrampNum();
+			System.out.println(pp);
 			System.out.println("プレイヤーは" + player + "を引きました");
-		}else{}
-		// for (Tramp a : tlist) {
-		// System.out.println(a);
-		// }
-		// for (Tramp a : plist) {
-		// System.out.println(a);
-		// }
+			if (pp > 21) {
+				System.out.println("バースト（負け）");
+				break;
+			}
+			System.out.println("カードを引きますか？（はい：yを入力、いいえ：それ以外を入力）");
+		}
+		while (dp < 17) {
+			Tramp dealer = tlist.remove(0);
+			dlist.add(dealer);
+			dp = dp + dlist.remove(0).getTrampNum();
+			System.out.println("ディーラーが引いたカードは非表示です。");
+		}
+		System.out.println(dp);
 		sc.close();
 	}
-
 }
