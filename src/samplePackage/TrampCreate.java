f37bf836db586c9a11812a9e97c9f8c7c944e9ca
dfjkyrcq <te.yuusuke.130.ushio.350@gmail.com>
2018-08-07 14:53:09 +0900

    トランプクラスの作成

---
+++ src/samplePackage/TrampCreate.java
@@ -0,0 +1,36 @@
+package samplePackage;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TrampCreate {
+	private static List<Tramp> trampbundle = new ArrayList<Tramp>();
+	private static Tramp[] tramp = Tramp.values();
+
+	public void setTrampBundle(List<Tramp> trampbundle) {
+		this.trampbundle = trampbundle;
+	}
+
+	public void setTramp(Tramp[] tramp) {
+		this.tramp = tramp;
+	}
+
+	public static List<Tramp> getTrampBundle() {
+		return trampbundle;
+	}
+
+	public static Tramp[] getTramp() {
+		return tramp;
+	}
+
+	public static void trampBundleCreate() {
+		for (int i = 0; i < tramp.length; i++) {
+			trampbundle.add(tramp[i]);
+		}
+	}
+
+	public static void trampShuffle() {
+		Collections.shuffle(trampbundle);
+	}
+}
