7b5f32af7f8edf03899c9a8699f95a675704125b
dfjkyrcq <te.yuusuke.130.ushio.350@gmail.com>
2018-08-07 16:55:47 +0900

    ディーラークラスの作成途中

--- src/samplePackage/Dealer.java
+++ src/samplePackage/Dealer.java
@@ -1,5 +1,24 @@
 package samplePackage;
 
-public class Dealer {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Dealer extends Player {
+	private List<Tramp> dealerhand = new ArrayList<Tramp>();
+	private int dealerpoint;
+	private Tramp dealer;
+	private String dealername = "ディーラー";
+
+	public void dealerSecondMessage() {
+		System.out.println("2枚目" + name + "が引いたカードは非表示です。");
+	}
+
+	public void dealerDraw() {
+		while (dealerpoint < 17) {
+			Player.isDraw(dealer, dealerhand);
+			Player.isPoint(dealerpoint, dealerhand);
+			Player.drawMessage(dealername, dealer);
+			Player.pointMessage(dealername, dealerpoint);
+		}
+	}
 }
