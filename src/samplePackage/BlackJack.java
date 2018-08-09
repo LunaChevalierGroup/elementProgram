8928dacb894283cfe79aabe822b0b701818c9176
dfjkyrcq <te.yuusuke.130.ushio.350@gmail.com>
2018-08-09 09:58:51 +0900

    ブラックジャッククラスの作成途中

--- src/samplePackage/BlackJack.java
+++ src/samplePackage/BlackJack.java
@@ -2,4 +2,14 @@ package samplePackage;
 
 public class BlackJack {
 
+	public void BattleJudge(int playerpoint, int dealerpoint) {
+
+		if (playerpoint >= dealerpoint) {
+			System.out.println("プレイヤーの勝ち");
+		} else {
+			System.out.println("プレイヤーの負け");
+		}
+
+	}
+
 }
