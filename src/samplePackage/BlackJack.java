b980f22b755a41d34e05b392383cdcfa8a1945b8
dfjkyrcq <te.yuusuke.130.ushio.350@gmail.com>
2018-08-09 17:00:50 +0900

    ブラックジャッククラスの作成途中

--- src/samplePackage/BlackJack.java
+++ src/samplePackage/BlackJack.java
@@ -1,6 +1,20 @@
 package samplePackage;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class BlackJack {
+	Scanner sc = new Scanner(System.in);
+
+	public void isBurst(int playerpoint, int dealerpoint) {
+		if (playerpoint > 21) {
+			System.out.println("プレイヤーがバースト(プレイヤーの負け)");
+			System.exit(0);
+		} else if (dealerpoint > 21) {
+			System.out.println("ディーラーがバースト(プレイヤーの勝ち)");
+			System.exit(0);
+		}
+	}
 
 	public void BattleJudge(int playerpoint, int dealerpoint) {
 
@@ -10,6 +24,38 @@ public class BlackJack {
 			System.out.println("プレイヤーの負け");
 		}
 
+	}
+
+	public static void isDraw(Tramp drawcard, List<Tramp> hand) {
+		drawcard = TrampCreate.getTrampBundle().remove(0);
+		hand.add(drawcard);
+	}
+
+	public static void isPoint(int point, List<Tramp> hand) {
+		point += hand.remove(0).getTrampPoint();
+	}
+
+	public static void drawMessage(String humanname, Tramp trampname) {
+		System.out.println(humanname + "は" + trampname + "を引きました");
+	}
+
+	public static void pointMessage(String humanname, int point) {
+		System.out.println(humanname + "のポイントは" + point + "です。");
+	}
+
+	public void isDrawSelectMessage() {
+		System.out.println("カードを引きますか？(はい：yを入力、いいえ：それ以外を入力)");
+	}
+
+	public void isDrawRepeat() {
+		while (sc.next().equals("y")) {
+			isDraw(Player.getPlayerDrawCard(), Player.getPlayerHand());
+			isPoint(Player.getPlayerPoint(), Player.getPlayerHand());
+			drawMessage(Player.getPlayerName(), Player.getPlayerDrawCard());
+			pointMessage(Player.getPlayerName(), Player.getPlayerPoint());
+			isBurst(Player.getPlayerPoint(),Dealer.getDealerPoint());
+			isDrawSelectMessage();
+		}
 	}
 
 }
---
+++ src/samplePackage/BlackJackMain.java
@@ -0,0 +1,11 @@
+package samplePackage;
+
+public class BlackJackMain {
+
+	public static void main(String[] args) {
+		Player.setPlayerPoint(0);
+		Dealer.setDealerPoint(0);
+
+	}
+
+}
