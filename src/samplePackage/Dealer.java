55f8c411d192b5e11274d1ab2a5f46e9742c7a0b
dfjkyrcq <te.yuusuke.130.ushio.350@gmail.com>
2018-08-09 17:08:35 +0900

    ディーラークラスの修正

--- src/samplePackage/Dealer.java
+++ src/samplePackage/Dealer.java
@@ -4,21 +4,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Dealer {
-	private List<Tramp> dealerhand = new ArrayList<Tramp>();
-	private int dealerpoint;
-	private Tramp dealer;
-	private String dealername = "ディーラー";
+	private static List<Tramp> dealerhand = new ArrayList<Tramp>();
+	private static int dealerpoint;
+	private static Tramp dealerdrawcard;
+	private final static String dealername = "ディーラー";
 
-	public void dealerSecondMessage() {
+	public static void setDealerHand(List<Tramp> dealerhand) {
+		Dealer.dealerhand = dealerhand;
+	}
+
+	public static void setDealerPoint(int dealerpoint) {
+		Dealer.dealerpoint = dealerpoint;
+	}
+
+	public void setDealerDrawCrad(Tramp dealerdrawcard) {
+		Dealer.dealerdrawcard = dealerdrawcard;
+	}
+
+	public static void dealerSecondMessage() {
 		System.out.println("2枚目" + dealername + "が引いたカードは非表示です。");
+	}
+
+	public static List<Tramp> getDealerhand() {
+		return dealerhand;
+	}
+
+	public static int getDealerPoint() {
+		return dealerpoint;
+	}
+
+	public Tramp getDealerDrawCard() {
+		return dealerdrawcard;
+	}
+
+	public String getDealerName() {
+		return dealername;
 	}
 
 	public void dealerDraw() {
 		while (dealerpoint < 17) {
-			Player.isDraw(dealer, dealerhand);
-			Player.isPoint(dealerpoint, dealerhand);
-			Player.drawMessage(dealername, dealer);
-			Player.pointMessage(dealername, dealerpoint);
+			BlackJack.isDraw(dealerdrawcard, dealerhand);
+			BlackJack.isPoint(dealerpoint, dealerhand);
+			BlackJack.drawMessage(dealername, dealerdrawcard);
+			BlackJack.pointMessage(dealername, dealerpoint);
 		}
 	}
 }
