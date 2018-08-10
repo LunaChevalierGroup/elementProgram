49bc88104d3481508f9e8579634f91fbd76a7f34
dfjkyrcq <te.yuusuke.130.ushio.350@gmail.com>
2018-08-10 17:05:45 +0900

    ブラックジャッククラスの作成

--- src/samplePackage/BlackJack.java
+++ src/samplePackage/BlackJack.java
@@ -1,22 +1,43 @@
 package samplePackage;
 
 import java.util.List;
-import java.util.Scanner;
 
-public class BlackJack {
-	Scanner sc = new Scanner(System.in);
+public class BlackJack extends TrampCreate {
 
-	public void isBurst(int playerpoint, int dealerpoint) {
-		if (playerpoint > 21) {
-			System.out.println("プレイヤーがバースト(プレイヤーの負け)");
-			System.exit(0);
-		} else if (dealerpoint > 21) {
-			System.out.println("ディーラーがバースト(プレイヤーの勝ち)");
+	public Tramp isDraw(Tramp drawcard, List<Tramp> hand) {
+		drawcard = getTrampBundle().remove(0);
+		hand.add(drawcard);
+		return drawcard;
+	}
+
+	public int isPoint(int point, List<Tramp> hand) {
+		point += hand.remove(0).getTrampPoint();
+		return point;
+	}
+
+	public void drawMessage(String humanname, Tramp drawcard) {
+		System.out.println(humanname + "は" + drawcard + "を引きました");
+	}
+
+	public void pointMessage(String humanname, int point) {
+		System.out.println(humanname + "のポイントは" + point + "です。");
+	}
+
+	public void isBurst(int point,String humanname) {
+		if (point > 21) {
+			lineShow();
+			System.out.println(humanname+"がバースト");
 			System.exit(0);
 		}
 	}
 
-	public void BattleJudge(int playerpoint, int dealerpoint) {
+	public void pointShow(int playerpoint, int dealerpoint) {
+		lineShow();
+		System.out.println("プレイヤーのポイントは" + playerpoint + "です。");
+		System.out.println("ディーラーのポイントは" + dealerpoint + "です。");
+	}
+
+	public void battleJudge(int playerpoint, int dealerpoint) {
 
 		if (playerpoint >= dealerpoint) {
 			System.out.println("プレイヤーの勝ち");
@@ -26,36 +47,7 @@ public class BlackJack {
 
 	}
 
-	public static void isDraw(Tramp drawcard, List<Tramp> hand) {
-		drawcard = TrampCreate.getTrampBundle().remove(0);
-		hand.add(drawcard);
+	public void lineShow() {
+		System.out.println("------------------------------------------------");
 	}
-
-	public static void isPoint(int point, List<Tramp> hand) {
-		point += hand.remove(0).getTrampPoint();
-	}
-
-	public static void drawMessage(String humanname, Tramp trampname) {
-		System.out.println(humanname + "は" + trampname + "を引きました");
-	}
-
-	public static void pointMessage(String humanname, int point) {
-		System.out.println(humanname + "のポイントは" + point + "です。");
-	}
-
-	public void isDrawSelectMessage() {
-		System.out.println("カードを引きますか？(はい：yを入力、いいえ：それ以外を入力)");
-	}
-
-	public void isDrawRepeat() {
-		while (sc.next().equals("y")) {
-			isDraw(Player.getPlayerDrawCard(), Player.getPlayerHand());
-			isPoint(Player.getPlayerPoint(), Player.getPlayerHand());
-			drawMessage(Player.getPlayerName(), Player.getPlayerDrawCard());
-			pointMessage(Player.getPlayerName(), Player.getPlayerPoint());
-			isBurst(Player.getPlayerPoint(),Dealer.getDealerPoint());
-			isDrawSelectMessage();
-		}
-	}
-
 }
--- src/samplePackage/BlackJackMain.java
+++ src/samplePackage/BlackJackMain.java
@@ -3,8 +3,42 @@ package samplePackage;
 public class BlackJackMain {
 
 	public static void main(String[] args) {
-		Player.setPlayerPoint(0);
-		Dealer.setDealerPoint(0);
+		Player player = new Player();
+		Dealer dealer = new Dealer();
+		BlackJack blackjack = new BlackJack();
+		player.setPlayerPoint(0);
+		dealer.setDealerPoint(0);
+
+		blackjack.trampBundleCreate();
+		blackjack.trampShuffle();
+
+		for (int i = 1; i <= 2; i++) {
+			player.setPlayerDrawCard(player.isDraw(player.getPlayerDrawCard(), player.getPlayerHand()));
+			player.setPlayerPoint(player.isPoint(player.getPlayerPoint(), player.getPlayerHand()));
+			player.drawMessage(player.getPlayerName(), player.getPlayerDrawCard());
+			player.pointMessage(player.getPlayerName(), player.getPlayerPoint());
+			player.lineShow();
+		}
+
+		dealer.setDealerDrawCard(dealer.isDraw(dealer.getDealerDrawCard(), dealer.getDealerHand()));
+		dealer.setDealerPoint(dealer.isPoint(dealer.getDealerPoint(), dealer.getDealerHand()));
+		dealer.drawMessage(dealer.getDealerName(), dealer.getDealerDrawCard());
+		dealer.pointMessage(dealer.getDealerName(), dealer.getDealerPoint());
+		dealer.lineShow();
+
+		dealer.setDealerDrawCard(dealer.isDraw(dealer.getDealerDrawCard(), dealer.getDealerHand()));
+		dealer.setDealerPoint(dealer.isPoint(dealer.getDealerPoint(), dealer.getDealerHand()));
+		dealer.dealerSecondMessage();
+		dealer.lineShow();
+
+		player.isDrawSelectMessage();
+		player.isDrawRepeat();
+
+		dealer.dealerDraw();
+		dealer.isBurst(player.getPlayerPoint(), dealer.getDealerName());
+
+		blackjack.pointShow(player.getPlayerPoint(), dealer.getDealerPoint());
+		blackjack.battleJudge(player.getPlayerPoint(), dealer.getDealerPoint());
 
 	}
 
