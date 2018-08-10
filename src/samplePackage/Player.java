4f2612a48bd07583b7361e6f47a59d6b94267b30
dfjkyrcq <te.yuusuke.130.ushio.350@gmail.com>
2018-08-10 17:16:14 +0900

    プレイヤークラスの作成

--- src/samplePackage/Player.java
+++ src/samplePackage/Player.java
@@ -2,39 +2,58 @@ package samplePackage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-public class Player {
-	private static List<Tramp> playerhand = new ArrayList<Tramp>();
-	private static int playerpoint;
-	private static Tramp playerdrawcard;
-	private final static String playername = "プレイヤー";
+public class Player extends BlackJack {
+	private List<Tramp> playerhand = new ArrayList<Tramp>();
+	private int playerpoint;
+	private Tramp playerdrawcard;
+	private final String playername = "プレイヤー";
 
 	public void setPlayerHand(List<Tramp> playerhand) {
-		Player.playerhand =playerhand;
+		this.playerhand = playerhand;
 	}
 
-	public static void setPlayerPoint(int playerpoint) {
-		Player.playerpoint = playerpoint;
+	public void setPlayerPoint(int playerpoint) {
+		this.playerpoint = playerpoint;
 	}
 
 	public void setPlayerDrawCard(Tramp playerdrawcard) {
-		Player.playerdrawcard = playerdrawcard;
+		this.playerdrawcard = playerdrawcard;
 	}
 
-	public static List<Tramp> getPlayerHand() {
+	public List<Tramp> getPlayerHand() {
 		return playerhand;
 	}
 
-	public static int getPlayerPoint() {
+	public int getPlayerPoint() {
 		return playerpoint;
 	}
 
-	public static Tramp getPlayerDrawCard() {
+	public Tramp getPlayerDrawCard() {
 		return playerdrawcard;
 	}
 
-	public static String getPlayerName() {
+	public String getPlayerName() {
 		return playername;
+	}
+
+	public void isDrawSelectMessage() {
+		System.out.println("カードを引きますか？(はい：yを入力、いいえ：それ以外を入力)");
+	}
+
+	public void isDrawRepeat() {
+		Scanner sc = new Scanner(System.in);
+		while (sc.next().equals("y")) {
+			setPlayerDrawCard(isDraw(playerdrawcard, playerhand));
+			setPlayerPoint(isPoint(playerpoint, playerhand));
+			drawMessage(playername, playerdrawcard);
+			pointMessage(playername, playerpoint);
+			isBurst(playerpoint,playername);
+			lineShow();
+			isDrawSelectMessage();
+		}
+		sc.close();
 	}
 
 }
